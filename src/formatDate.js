7f406c3e9fc06b5cd797dcc3c6ed89fe4921cf26
MorelSami <mksami237@gmail.com>
2025-03-03 02:28:14 -0600

    solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -9,6 +9,42 @@
  */
 function formatDate(date, fromFormat, toFormat) {
   // write code here
+  const partitions = [];
+
+  for (let i = 0; i < 3; i++) {
+    partitions[fromFormat[i]] = date.split(fromFormat.at(-1))[i];
+  }
+
+  let newFormat = '';
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    let pDate = '';
+
+    switch (toFormat[i]) {
+      case 'MM':
+      case 'DD':
+        pDate = partitions[toFormat[i]];
+        break;
+      default:
+        if (toFormat[i].length === 2) {
+          pDate = partitions[toFormat[i]].slice(-1, -3);
+        } else {
+          pDate =
+            partitions[toFormat[i]] < 30
+              ? `19${partitions[toFormat[i]]}`
+              : `20${partitions[toFormat[i]]}`;
+        }
+        break;
+    }
+
+    newFormat +=
+      i + 1 === toFormat.length ? `${pDate}` : `${pDate}${toFormat.at(-1)}`;
+  }
+
+  /* eslint no-console: ["error", { allow: ["warn", "log"] }] */
+  console.log(newFormat);
+
+  return newFormat;
 }
 
 module.exports = formatDate;
